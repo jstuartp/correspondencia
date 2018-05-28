c829676089ffca14ff6f91cd09874ce6399509e7
Stuart <jstuartp@gmail.com>
2018-05-28 10:36:58 -0600

    Trabajo del 28 de mayo

---
+++ includes/encabezado1.php
@@ -0,0 +1,12 @@
+<link href="../bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+<div id="encabezadoDelSistema" class="encabezadoDelSistema">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><div class="col-lg-3 col-sm-3 col-md-3">
+                      <a href="http://www.ucr.ac.cr/" target="_blank" title="Página principal de la Universidad de Costa Rica">
+                        <img src="https://cdn.ucr.ac.cr/Portal/css/images/firma-ucr.svg" class="img-responsive" alt="Logo de la Universidad de Costa Rica" role="presentation" />
+                    </a></div><div class="col-lg-3 col-sm-3 col-md-3"> 
+                    <a href="http://www.fmedicina.ucr.ac.cr/" target="_blank" title="Página principal de la Universidad de Costa Rica">
+                        <img src="imagenes/acronimo.png" class="img-responsive" alt="Logo Facultado de Medicina" role="presentation" />
+                    </a></div>
+  </div>
+</div>
+
